bccc89bc236ba7f1e860b19c8a530567d5c4493c
Michael McEniry [UAH] <mcenirm@uah.edu>
2019-05-11 22:38:00 -0500

    One table

--- junk.go
+++ junk.go
@@ -12,57 +12,43 @@ import (
 )
 
 const (
-	schemainode = `create table if not exists
-		xinode (
+	schemalink = `create table if not exists
+		xlink (
+		dir text,
+		nam text,
 		dev integer,
 		ino integer,
 		siz integer,
 		mod integer
 		)`
-	insertinode = `insert into
-		xinode (
+	insertlink = `insert into
+		xlink (
+		dir,
+		nam,
 		dev,
 		ino,
 		siz,
 		mod
 		) values (
+		:dir,
+		:nam,
 		:dev,
 		:ino,
 		:siz,
 		:mod
 		)`
-	schemalink = `create table if not exists
-		xlink (
-		dir text,
-		nam text,
-		ino integer
-		)`
-	insertlink = `insert into
-		xlink (
-		dir,
-		nam,
-		ino
-		) values (
-		:dir,
-		:nam,
-		:ino
-		)`
 )
 
-type xinode struct {
+type xlink struct {
+	dir string
+	nam string
 	dev int32
 	ino uint64
 	siz int64
 	mod uint16
 }
 
-type xlink struct {
-	dir string
-	nam string
-	ino uint64
-}
-
-func mkwf(stmtinode *sqlx.NamedStmt, stmtlink *sqlx.NamedStmt) filepath.WalkFunc {
+func mkwf(stmtlink *sqlx.NamedStmt) filepath.WalkFunc {
 	startpathIsSet := false
 	startpath := ""
 	counter := 0
@@ -81,19 +67,17 @@ func mkwf(stmtinode *sqlx.NamedStmt, stmtlink *sqlx.NamedStmt) filepath.WalkFunc
 
 		nam := info.Name()
 		st := info.Sys().(*syscall.Stat_t)
-		inode := xinode{st.Dev, st.Ino, st.Size, st.Mode}
 		left := len(startpath) + 1
 		right := len(path) - len(nam) - 1
 		dir := ""
 		if left < right {
 			dir = path[left:right]
 		}
-		link := xlink{dir, nam, st.Ino}
+		link := xlink{dir, nam, st.Dev, st.Ino, st.Size, st.Mode}
 
-		stmtinode.MustExec(&inode)
 		stmtlink.MustExec(&link)
 
-		fmt.Fprintf(os.Stderr, "%x %8x %6o %9d %q %q\n", inode.dev, inode.ino, inode.mod, inode.siz, link.dir, link.nam)
+		fmt.Fprintf(os.Stderr, "%x %8x %6o %9d %q %q\n", link.dev, link.ino, link.mod, link.siz, link.dir, link.nam)
 		counter++
 		if counter > 10 {
 			return io.EOF
@@ -119,17 +103,10 @@ func main() {
 		panic(e)
 	}
 
-	db.MustExec(schemainode)
 	db.MustExec(schemalink)
 
 	tx := db.MustBegin()
 	defer tx.Commit()
-
-	stmtinode, e := tx.PrepareNamed(insertinode)
-	if e != nil {
-		panic(e)
-	}
-	defer stmtinode.Close()
 
 	stmtlink, e := tx.PrepareNamed(insertlink)
 	if e != nil {
@@ -137,7 +114,7 @@ func main() {
 	}
 	defer stmtlink.Close()
 
-	e = filepath.Walk(startpath, mkwf(stmtinode, stmtlink))
+	e = filepath.Walk(startpath, mkwf(stmtlink))
 	if e == io.EOF {
 		return
 	}
